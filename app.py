55d8fa213dcfc5369a631df937ed3f5659431f81
Meharpreet Singh Nanda <msnanda515@gmail.com>
2022-03-05 22:30:10 +0000

    Add pydeck for 3d map

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pydeck as pdk
 
 DATA_URL = (
     "/home/rhyme/Desktop/Project/Motor_Vehicle_Collisions_-_Crashes.csv"
@@ -23,8 +24,23 @@ data = load_data(100000)
 
 st.header("Where are the most people injured in NYC?")
 injured_people = st.slider("Number of people injured in vehicle collisions", 0, 19)
-st.map(data.query("injured_persons >= @injured_people")[["latitude", "longitude"]].dropna(how="any")) 
+st.map(data.query("injured_persons >= @injured_people")[["latitude", "longitude"]].dropna(how="any"))
 
+st.header("How many collisions occur during a given time of day?")
+hour = st.slider("Hour to look at", 0, 23)
+data = data[data['date/time'].dt.hour == hour]
+
+st.markdown("Vehicle collisions between %i:00 and %i:00" %(hour, (hour+1) % 24))
+midpoint = (np.average(data['latitude']), np.average(data['longitude']))
+st.write(pdk.Deck(
+    map_style="mapbox://styles/mapbox/light-v9",
+    initial_view_state={
+        "latitude": midpoint[0],
+        "longitude": midpoint[1],
+        "zoom": 11,
+        "pitch": 50,
+    },
+))
 
 if st.checkbox("Show Raw Data", False):
     st.subheader("Raw Data")
