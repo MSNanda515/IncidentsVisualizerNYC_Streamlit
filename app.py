17ca9c395e547ba6087a8d024b1aeabc29c1130d
Meharpreet Singh Nanda <msnanda515@gmail.com>
2022-03-05 21:59:51 +0000

    Add reading and preprocessing of data

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("Hello World!")
-st.markdown("### My first dashboard")
+DATA_URL = (
+    "/home/rhyme/Desktop/Project/Motor_Vehicle_Collisions_-_Crashes.csv"
+)
+
+st.title("Motor Vehicle Collisions in New York City")
+st.markdown("This application is a Streamline dashboard that can be used" +
+"to analyze motor vehicle collisions in NYC 🗽💥🚙")
+
+@st.cache(persist=True)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
+    data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
+    lowercase = lambda x : str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data.rename(columns={'crash_data_crash_time': 'data/time'}, inplace=True)
+    return DATA_URL
+
+data = load_data(100000)
